102df993d627d952159d3384f3f6f542f83da2e9
Diako Amir <sabina.diako@gmail.com>
2020-01-20 16:24:59 +0300

    refactor record

--- engine/engine.go
+++ engine/engine.go
@@ -1,12 +1,9 @@
 package engine
 
 import (
-	"encoding/json"
 	"fmt"
 	"omega/config"
-	"omega/internal/models"
 
-	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 )
@@ -53,69 +50,4 @@ func (e *Engine) CheckInfo(err error, message string) {
 			"err": err.Error(),
 		}).Info(message)
 	}
-}
-
-// Record an activity
-func (e *Engine) Record(c *gin.Context, event string, data ...interface{}) {
-	var before, after []byte
-	var err error
-	var userID uint64
-	var username string
-
-	type RecordType int
-	const (
-		read RecordType = iota
-		write
-	)
-	var recordType RecordType = read
-
-	num := len(data)
-	if num > 0 {
-		if data[0] != nil {
-			before, err = json.Marshal(data[0])
-			e.CheckError(err, "error in encoding data to before-json")
-		}
-		recordType = write
-	}
-
-	switch recordType {
-	case read:
-		if !e.Environments.Setting.RecordRead {
-			return
-		}
-	default:
-		if !e.Environments.Setting.RecordRead {
-			return
-		}
-	}
-
-	if num > 1 {
-		after, err = json.Marshal(data[1])
-		e.CheckError(err, "error in encoding data to after-json")
-	}
-
-	userIDtmp, ok := c.Get("USER_ID")
-	if ok {
-		userID = userIDtmp.(uint64)
-	}
-
-	usernameTmp, ok := c.Get("USERNAME")
-	if ok {
-		username = usernameTmp.(string)
-	}
-
-	activity := models.Activity{
-		Event:    event,
-		UserID:   userID,
-		IP:       c.ClientIP(),
-		Path:     c.Request.URL.Path,
-		Before:   string(before),
-		After:    string(after),
-		Username: username,
-	}
-
-	err = e.ActivityDB.Save(&activity).Error
-	e.CheckInfo(err, fmt.Sprintf("Failed in saving activity for %+v", activity))
-
-	return
 }
